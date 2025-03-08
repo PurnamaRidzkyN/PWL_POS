b514348562da22d00e0ca72224d7eafbbf68fe0e
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-03-08 18:20:21 +0700

    one to one

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,31 +11,42 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('1234')
-        ];
-
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('1234'),
-                'level_id' => 2
-
-            ],
-        );
-        $user->username = 'manager12';
-
-        $user->save();
-
-        $user->wasChanged();
-        $user->wasChanged('username');
-        $user->wasChanged(['username','level_id']);
-        $user->wasChanged('nama');
-        dd($user->wasChanged(['nama', 'username']));
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
+    }
+    function tambah()
+    {
+        return view('user_tambah');
+    }
+    function tambah_simpan(Request $req)
+    {
+        UserModel::create([
+            'username' => $req->username,
+            'nama' => $req->nama,
+            'password' => Hash::make('$req->password'),
+            'level_id' => $req->level_id
+        ]);
+        return redirect('/user');
+    }
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    public function ubah_simpan($id, Request $req)
+    {
+        $user = UserModel::find($id);
+        $user->username = $req->username;
+        $user->nama = $req->nama;
+        $user->password = Hash::make('$req->password');
+        $user->level_id = $req->level_id;
+        $user->save();
+        return redirect('/user');
+    }
+
+    public function hapus($id){
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserModel extends Model
 {
@@ -12,4 +14,9 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
