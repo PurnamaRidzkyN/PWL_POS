f7ecf571106650fbe6c1de69aeaeadfc401cc1e7
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-04-19 20:56:15 +0700

    Tugas 2 Praktikum 2 Jobsheet 8

--- 1_kode_dari_jobsheet_7/routes/web.php
+++ 1_kode_dari_jobsheet_7/routes/web.php
@@ -112,6 +112,7 @@ Route::middleware(['auth'])->group(function () {
             Route::post('/ajax', [BarangController::class, 'store_ajax']);
             Route::get('/import', [BarangController::class, 'import']);
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+            Route::get('/export_excel', [BarangController::class, 'export_excel']);
             Route::get('/{id}', [BarangController::class, 'show']);
             Route::get('/{id}/edit', [BarangController::class, 'edit']);
             Route::put('/{id}', [BarangController::class, 'update']);
