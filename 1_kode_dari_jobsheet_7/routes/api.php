e23d79f1d3cfb05d580ff3a59c8a1a422a08f517
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-05-04 14:58:32 +0700

    Praktikum 4 jobsheet 10

--- 1_kode_dari_jobsheet_7/routes/api.php
+++ 1_kode_dari_jobsheet_7/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,29 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+
+Route::get('levels', [LevelController::class , 'index']);
+Route::post('levels', [LevelController::class , 'store']);
+Route::get('levels/{level}', [LevelController::class , 'show']);
+Route::put('levels/{level}', [LevelController::class , 'update']);
+Route::delete('levels/{level}', [LevelController::class , 'destroy']);
+
+
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+Route::get('kategori', [KategoriController::class, 'index']);
+Route::post('kategori', [KategoriController::class, 'store']);
+Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+Route::get('barang', [BarangController::class, 'index']);
+Route::post('barang', [BarangController::class, 'store']);
+Route::get('barang/{barang}', [BarangController::class, 'show']);
+Route::put('barang/{barang}', [BarangController::class, 'update']);
+Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
