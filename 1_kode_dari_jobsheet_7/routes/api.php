2f433e3c9adaf6a3fb1858d1effd4a30dcd6c75c
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-05-04 15:51:09 +0700

    Tugas jobsheet 10

--- 1_kode_dari_jobsheet_7/routes/api.php
+++ 1_kode_dari_jobsheet_7/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
